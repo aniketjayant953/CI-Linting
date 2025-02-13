ad2689eec6a779dbd1855a021de71ca6e64fd8ef
aniketjayant953 <aniketjayant953@gmail.com>
2025-02-13 13:18:46 +0530

    add linting and testing phase

---
+++ app.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    results1 = add(1, 2)
+    results2 = sub(2, 3)
+
+    print(results1, results2)
